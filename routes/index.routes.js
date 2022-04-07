d846610b5c6b8850c91e2264e548c863d6c8a256
Patricia <patriciasilva@Air-de-Patricia.home>
2022-04-07 17:34:02 +0100

    allproducts route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,25 +4,15 @@ const router = require("express").Router();
 const authRoutes = require("./auth.routes");
 const profileRoutes = require("./profile.routes");
 const alertRoutes = require("./alert.routes");
+const productsRoutes = require("./products.routes");
 
-const Product = require("../models/product.model");
+
 
 /* GET home page */ // http://localhost:5005/api
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-//http://localhost:5005/api/home
-router.get("/home", async (req, res, next) => {
-  try{
-      const productsAvailable = await Product.find();
-      res.json({ productsAvailable });
-  } catch (err) {
-      res.status(400).json({
-        errorMessage: "Error fetching doners lists from server! " + err.message,
-      });
-    }
-})
 
 router.get("/getCsrfToken", csrfMiddleware, (req, res, next) => {
   res.json({ csrfToken: req.csrfToken() });
@@ -32,5 +22,6 @@ router.get("/getCsrfToken", csrfMiddleware, (req, res, next) => {
 router.use("/auth", authRoutes);
 router.use("/profile", profileRoutes)
 router.use("/checkproduct", alertRoutes)
+router.use("/allproducts", productsRoutes)
 
 module.exports = router;
