ca3e1d535143282539a4d4ade3640e46e93ef5c0
DaianyNascimento <daiany.cristina@outlook.com>
2022-04-13 16:29:13 +0100

    adjustments

--- routes/homeProducts.routes.js
+++ routes/homeProducts.routes.js
@@ -9,15 +9,10 @@ const Product = require("../models/product.model");
 const Alert = require("../models/alert.model");
 
 
-router.get("/", csrfMiddleware, isLoggedIn, async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const productsAvailable = await Product.find();
-
-    const pendingAlerts = await Alert.find({donor: req.session.user._id} ).populate("donee donor product");
-    //console.log("This is body from pending aaalerts", req.session.user._id)
-    //console.log("This is pendingAlerts from the get route", pendingAlerts)
-
-    res.json({ productsAvailable, pendingAlerts });
+    res.json({ productsAvailable });
   } catch (err) {
     res.status(400).json({
       errorMessage: "Error fetching products and alerts lists from server! " + err.message,
@@ -25,34 +20,46 @@ router.get("/", csrfMiddleware, isLoggedIn, async (req, res, next) => {
   }
 })
 
+router.get("/alerts", csrfMiddleware, isLoggedIn, async (req, res, next) => {
+  try {
+    const pendingAlerts = await Alert.find({ donor: req.session.user._id }).populate("donee donor product");
+
+    res.json({ pendingAlerts });
+  } catch (err) {
+    res.status(400).json({
+      errorMessage: "Error fetching alerts from server! " + err.message,
+    });
+  }
+})
+
 router.post("/:id", isLoggedIn, isDonee, async (req, res, next) => {
   try {
-      const productId = req.params.id;
-      //console.log("params", req.params.id)
-      //console.log("productId", productId)
+    const productId = req.params.id;
+    //console.log("params", req.params.id)
+    //console.log("productId", productId)
 
-      const doneeId = req.session.user._id;
-      //console.log("sessions", req.session.user._id)
-      //console.log("This is the doneeId", doneeId)
-      
-      const productRequested = await Product.findById({_id: productId});
-      //console.log("productRequested", productRequested)
+    const doneeId = req.session.user._id;
+    //console.log("sessions", req.session.user._id)
+    //console.log("This is the doneeId", doneeId)
 
-      const donorAlert = await Donor.findOne({_id : productRequested.donor});
+    const productRequested = await Product.findById({ _id: productId });
+    //console.log("productRequested", productRequested)
 
-      //console.log("This is donorAlert", donorAlert)
+    const donorAlert = await Donor.findOne({ _id: productRequested.donor });
 
-      const newAlert = await Alert.create({ donee: doneeId, donor: donorAlert, product: productRequested});
-    
-      await newAlert.populate("donee donor product")
-      await newAlert.save();
+    //console.log("This is donorAlert", donorAlert)
 
-      //console.log("This is newAlert", newAlert)
-      res.json({newAlert : newAlert})
+    const newAlert = await Alert.create({ donee: doneeId, donor: donorAlert, product: productRequested });
+
+    await newAlert.populate("donee donor product")
+    await newAlert.save();
+
+    //console.log("This is newAlert", newAlert)
+    res.json({ newAlert: newAlert })
 
   } catch (error) {
     res.status(400).json({
-    errorMessage: "Error creating an alert!" + error.message,
+      errorMessage: "Error creating an alert!" + error.message,
     });
   }
 });
@@ -60,8 +67,8 @@ router.post("/:id", isLoggedIn, isDonee, async (req, res, next) => {
 module.exports = router;
 
 
-      
-      /*
 
-      donorToBeContacted.alertsList.push(newAlert._id);
-      await donorToBeContacted.save()*/
+/*
+
+donorToBeContacted.alertsList.push(newAlert._id);
+await donorToBeContacted.save()*/
