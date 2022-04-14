c110e4f8b657d0b724729c1d4b6cd1d0f684d13a
DaianyNascimento <daiany.cristina@outlook.com>
2022-04-14 16:25:37 +0100

    deploy

--- config/index.js
+++ config/index.js
@@ -23,6 +23,13 @@ module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
+  // Controls a very specific header to pass headers from the frontend
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN || "http://localhost:3000",
+    })
+  );
   const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/REUS";
   // Session & Cookies
   app.use(
@@ -42,13 +49,7 @@ module.exports = (app) => {
     })
   );
 
-  // Controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
-    })
-  );
+
 
   app.use(helmet());
 
