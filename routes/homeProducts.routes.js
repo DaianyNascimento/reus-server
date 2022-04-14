8d759bfbd39bba5b6936950daac220f7ed8d7ce3
DaianyNascimento <daiany.cristina@outlook.com>
2022-04-14 06:41:59 +0100

    bug fix

--- routes/homeProducts.routes.js
+++ routes/homeProducts.routes.js
@@ -8,7 +8,6 @@ const Donor = require("../models/donor.model");
 const Product = require("../models/product.model");
 const Alert = require("../models/alert.model");
 
-
 router.get("/", async (req, res, next) => {
   try {
     const productsAvailable = await Product.find();
@@ -23,7 +22,6 @@ router.get("/", async (req, res, next) => {
 router.get("/alerts", csrfMiddleware, isLoggedIn, async (req, res, next) => {
   try {
     const pendingAlerts = await Alert.find({ donor: req.session.user._id }).populate("donee donor product");
-
     res.json({ pendingAlerts });
   } catch (err) {
     res.status(400).json({
@@ -35,28 +33,15 @@ router.get("/alerts", csrfMiddleware, isLoggedIn, async (req, res, next) => {
 router.post("/:id", isLoggedIn, isDonee, async (req, res, next) => {
   try {
     const productId = req.params.id;
-    //console.log("params", req.params.id)
-    //console.log("productId", productId)
-
     const doneeId = req.session.user._id;
-    //console.log("sessions", req.session.user._id)
-    //console.log("This is the doneeId", doneeId)
-
     const productRequested = await Product.findById({ _id: productId });
-    //console.log("productRequested", productRequested)
-
     const donorAlert = await Donor.findOne({ _id: productRequested.donor });
-
-    //console.log("This is donorAlert", donorAlert)
-
     const newAlert = await Alert.create({ donee: doneeId, donor: donorAlert, product: productRequested });
 
     await newAlert.populate("donee donor product")
     await newAlert.save();
 
-    //console.log("This is newAlert", newAlert)
     res.json({ newAlert: newAlert })
-
   } catch (error) {
     res.status(400).json({
       errorMessage: "Error creating an alert!" + error.message,
@@ -65,10 +50,3 @@ router.post("/:id", isLoggedIn, isDonee, async (req, res, next) => {
 });
 
 module.exports = router;
-
-
-
-/*
-
-donorToBeContacted.alertsList.push(newAlert._id);
-await donorToBeContacted.save()*/
